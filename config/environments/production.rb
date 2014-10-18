48040ded9b68323a4ecc1ab1b9ea42d74069605c
Alexey Tataurov <tataurov@ek.apress.ru>
2014-10-18 20:26:26 +0600

    fix(application): add app variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Tataurovv::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-
+  config.assets.precompile += %w( application_admin.js application_admin.css )
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
@@ -59,7 +59,6 @@ Tataurovv::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( application_admin.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
